dbe784f32249de859224ef55ca6a1bbbcb686e7e
s0mmerfeld <sendenmeyouremail@gmail.com>
2022-08-23 23:29:32 +0300

    Add API controllers

---
+++ CryptoAPI/Controllers/CryptingAppController.cs
@@ -0,0 +1,58 @@
+﻿using CryptoAPI.BLL.Services;
+using CryptoAPI.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CryptoAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CryptingAppController : ControllerBase
+	{
+		private readonly CryptingAppAPI _cryptingAppAPI;
+		private readonly ILogger<CryptingAppController> _logger;
+		public CryptingAppController(CryptingAppAPI cryptingAppAPI, ILogger<CryptingAppController> logger)
+		{
+			_cryptingAppAPI = cryptingAppAPI;
+			_logger = logger;
+		}
+
+		[HttpGet]
+
+		public async Task<Root> GetProductComposition()
+		{
+
+			_logger.LogInformation("API Before API");
+			var crypt = await _cryptingAppAPI.GetAll();
+			_logger.LogInformation("API After API");
+			if (crypt == null)
+			{
+				_logger.LogInformation("NULL");
+				return null;
+			}
+			_logger.LogInformation("API After NULL");
+			return crypt;
+		}
+
+
+		[HttpGet("{size}")]
+
+		public async Task<Root> GetProductComposition(int size)
+		{
+
+			_logger.LogInformation("API Getting information about product");
+			var crypt = await _cryptingAppAPI.GetBySize(size);
+			if (crypt == null)
+			{
+				return null;
+			}
+			return crypt;
+		}
+
+	}
+
+}
--- CryptoAPI/Startup.cs
+++ CryptoAPI/Startup.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,8 +12,13 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+
+using CryptoAPI.BLL.Services;
 
 namespace CryptoAPI
 {
@@ -23,26 +31,89 @@ namespace CryptoAPI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddCors();
+            //Enable CORS
+            services.AddCors(c =>
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader())
+            );
+
+
+            //JSON SErializer
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+                .Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+                = new DefaultContractResolver());
+
+
             services.AddControllers();
+
+            services.AddHttpClient<CryptingAppAPI>();
+            services.AddSingleton<CryptingAppAPI>();
+
+
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CryptoAPI", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AutomatAPI", Version = "v1" });
             });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CryptoAPI v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AutomatAPI v1"));
             }
+
+            else
+            {
+                app.UseExceptionHandler(errorApp =>
+                {
+                    errorApp.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError; ;
+                        context.Response.ContentType = "text/html";
+
+                        await context.Response.WriteAsync("<html lang=\"en\"><body>\r\n");
+                        await context.Response.WriteAsync("ERROR!<br><br>\r\n");
+
+                        var exceptionHandlerPathFeature =
+                            context.Features.Get<IExceptionHandlerPathFeature>();
+
+                        if (exceptionHandlerPathFeature?.Error is FileNotFoundException)
+                        {
+                            logger.LogError($"Error: File error thrown!");
+                            await context.Response.WriteAsync(
+                                                      "File error thrown!<br><br>\r\n");
+                        }
+
+                        await context.Response.WriteAsync(
+                                                      "<a href=\"/\">Home</a><br>\r\n");
+                        await context.Response.WriteAsync("</body></html>\r\n");
+                        await context.Response.WriteAsync(new string(' ', 512));
+                    });
+                });
+                app.UseHsts();
+            }
+
+
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            app.UseHttpsRedirection();
+
 
             app.UseHttpsRedirection();
 
